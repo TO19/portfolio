1687d35da5204b961ccc1b55ec23110ccc20144d
Antonin <goix_a@etna-alternance.net>
2019-11-03 01:20:51 +0100

    Fix multiple style

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 window.onscroll = () => {
   progessBarOnScroll();
   scrollTopBtnHandle();
