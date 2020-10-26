935b6d906fcdb0cc814ee481e1eb7fe2d7267689
TX19 <rlf64990@gmail.com>
2020-10-26 11:05:15 +0100

    Home header

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement } from "react";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { useTheme } from "../shared/hooks/use-theme";
 
@@ -14,29 +14,12 @@ export const ThemeContext = React.createContext<ThemeProviderType>(
 
 export default function AppProviders(props: any): ReactElement {
   const [theme, type, handleThemeSwitch] = useTheme();
-  const [appLoading, setAppLoading] = useState(false);
 
   return (
     <ThemeContext.Provider value={{ theme, type, handleThemeSwitch }}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {appLoading ? (
-          <div
-            style={{
-              height: "100vh",
-              width: "100vw",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <span role="img" aria-label="Loading state">
-              🤔
-            </span>
-          </div>
-        ) : (
-          props.children
-        )}
+        {props.children}
       </ThemeProvider>
     </ThemeContext.Provider>
   );
--- src/shared/hooks/use-styles.tsx
+++ src/shared/hooks/use-styles.tsx
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles(() => ({
   headerTitle: {
     flexGrow: 1,
+    display: "flex",
     textShadow: "2px 4px 3px rgba(0,0,0,0.3)",
+    alignContent: "center",
   },
 }));
